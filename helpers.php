55e7a03f7a3b7b1ed455c0bd6fea244356c62e83
Abdulrahman <etwy.2000@gmail.com>
2026-03-09 00:10:08 +0300

    Adding helper methods

--- helpers.php
+++ helpers.php
@@ -11,3 +11,63 @@ function basePath($path = '')
 {
   return __DIR__ . '/' . $path;
 }
+
+/**
+ * Load a View
+ * 
+ * @param string $name
+ * @return void
+ */
+function loadView($name)
+{
+  $loadPathe = basePath('views/' . $name . '.view.php');
+
+  if (file_exists($loadPathe)) {
+    return $loadPathe;
+  } else {
+    die('View' . $name . 'Not Found');
+  }
+}
+
+/**
+ * Load a Partial
+ * 
+ * @param string $name
+ * @return void
+ */
+function loadPartial($name)
+{
+  $partialPath = basePath('views/partials/' . $name . '.php');
+
+  if (file_exists($partialPath)) {
+    require $partialPath;
+  } else {
+    die('Partial' . $name . 'Not Found');
+  }
+}
+
+/**
+ * Inspect a value(s)
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspect($value)
+{
+  echo '<pre>';
+  var_dump($value);
+  echo '</pre>';
+}
+
+/**
+ * Inspect a value(s) and die
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspectAndDie($value)
+{
+  echo '<pre>';
+  die(var_dump($value));
+  echo '</pre>';
+}
